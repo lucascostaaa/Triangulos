0b161be05e43739f15e10b1d26114ad26c674c4f
Lucas da Silva Costa <lucassilvacosta72@gmail.com>
2021-05-10 15:27:47 -0300

    Add project files.

---
+++ Triangulos.ConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace Triangulos.ConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           TipoTriangulo equilatero = new TipoTriangulo();
+           TipoTriangulo isoseles = new TipoTriangulo();
+           TipoTriangulo escaleno = new TipoTriangulo();
+
+            int x = 30;
+            int y = 30;
+            int z = 20;
+
+            if ((x < 0) || (y < 0) || (z < 0))
+            {
+
+                Console.WriteLine("\nOs valores não podem formar um triangulo\n");
+
+            }
+
+            else if ((x == y) && (x == z) && (y == z))
+            {
+
+                Console.WriteLine("O triangulo e equilátero\n");
+
+            }
+
+            else if ((x == y) && (x == z) || (y == z))
+            {
+
+                Console.WriteLine("O triangulo e isósceles\n");
+
+            }
+
+            else if ((x != y) && (x != z) && (y != z))
+            {
+
+                Console.WriteLine("O triangulo e escaleno\n");
+
+            }
+            else
+            {
+                Console.WriteLine("Triangulo Invalido");
+             
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Triangulos.ConsoleApp/TipoTriangulo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulos.ConsoleApp
+{
+   class TipoTriangulo
+   {
+        public int x = 0;
+        public int y = 0;
+        public int z = 0;
+
+
+        public TipoTriangulo()
+        {
+
+        }
+        public TipoTriangulo(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z; 
+        }
+
+        }
+        public string Tipo()
+        {
+            string tipoTriangulo = "";
+
+            if (Validacao())
+            {
+                if ((x == y) && (y == z))
+                {
+                    tipoTriangulo = "Equilátero";
+                }
+                else if ((x == y) || (x == z) || (y == z))
+                {
+                    tipoTriangulo = "Isósceles";
+                }
+                else
+                {
+                    tipoTriangulo = "Escaleno";
+                }
+            }
+            else
+            {
+                tipoTriangulo = "";
+            }
+            return tipoTriangulo;
+        }
+
+
+    }
+
+
+}
